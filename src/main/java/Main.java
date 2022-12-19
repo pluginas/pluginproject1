14367ec562536a736066e28d832eb2af1c7c8d50
pluginas.24 <sokola18@yandex.ru>
2022-12-19 14:47:14 +0300

    classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import app.Application;
+import io.github.humbleui.jwm.App;
 
+public class Main {
+
+    public static void main(String[] args) {
+        App.start(Application::new);
     }
+
 }
